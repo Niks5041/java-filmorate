c39a76f55e59398fe4d1cae554d48f0145162b69
Nikson <nik504441@yandex.ru>
2024-06-13 20:41:48 +0300

    1 версия 10 ТЗ

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,84 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exceptions.NotFoundException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    private final Map<Long, User> users = new HashMap<>();
+    @GetMapping
+    public ResponseEntity<Collection<User>> getAllUsers() {
+        Collection<User> allUsers = users.values();
+        return ResponseEntity.ok().body(allUsers);
+    }
+
+    @PostMapping
+    public ResponseEntity addNewUser(@Valid @RequestBody User user) {
+        if (user.getEmail() == null || !user.getEmail().contains("@")) {
+            log.info("Проверте правильность заполнения Email!");
+            throw new ValidationException("Ошибка!");
+        }
+        if (user.getLogin() == null || user.getLogin().isEmpty() || user.getLogin().isBlank()) {
+            log.info("Проверте правильность заполнения Login!");
+            throw new ValidationException("Ошибка!");
+        }
+        if (user.getName() == null || user.getName().isEmpty()) {
+            user.setName(user.getLogin());
+            log.info("В качестве имени будет использован логин");
+        }
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.info("Дата рождения не может быть в будущем!");
+            throw new ValidationException("Ошибка!");
+        }
+
+        user.setId(getNextId());
+        users.put(user.getId(), user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@Valid @RequestBody User updatedUser) {
+        User oldUser = users.get(updatedUser.getId());
+        if (oldUser == null) {
+            log.info("Пользователь не найден!");
+            throw new NotFoundException("Ошибка!");
+        }
+        if (updatedUser.getName() != null) {
+            oldUser.setName(updatedUser.getName());
+        }
+        if (updatedUser.getEmail() != null) {
+            oldUser.setEmail(updatedUser.getEmail());
+        }
+        if (updatedUser.getLogin() != null) {
+            oldUser.setLogin(updatedUser.getLogin());
+        }
+        if (updatedUser.getBirthday() != null) {
+            oldUser.setBirthday(updatedUser.getBirthday());
+        }
+
+        return ResponseEntity.ok().body(oldUser);
+    }
+
+    private long getNextId() {
+        long currentMaxId = users.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
-public class Film {
+@Data
+public class Film {                 //Добырй день! если аннотирую все поля то постман не проходит,если без аннотаций классов model - все ОК, я не верно аннатриую или проверки постман не расщитаны на использование данных аннотаций
+    Long id;
+    @NotNull
+    @NotBlank
+    String name;
+    @NotNull
+    @Size(max = 200)
+    String description;
+    @NotNull
+    LocalDate releaseDate;
+    @PositiveOrZero
+    Duration duration;
 }
