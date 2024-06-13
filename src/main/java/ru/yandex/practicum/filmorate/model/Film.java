671ae3e0c1f6bd79be7c48661063fe0ba69c26c7
Nikson <nik504441@yandex.ru>
2024-06-13 20:41:48 +0300

    1 версия 10 ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import java.time.LocalDate;
 
-/**
- * Film.
- */
-@Getter
-@Setter
-public class Film {
+@Data
+public class Film {                 //Добрый день! если аннотирую все поля аннотациями типа @NonNull по доп заданию то постман не проходит,если без аннотаций классов model - все ОК, я не верно аннатриую или проверки постман не расщитаны на использование данных аннотаций
+    Long id;
+    String name;
+    String description;
+    LocalDate releaseDate;
+    Integer duration;
 }
