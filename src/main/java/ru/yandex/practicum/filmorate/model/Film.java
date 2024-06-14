874c8ba279d4237bfce6716b65621febebc7fd37
Nikson <nik504441@yandex.ru>
2024-06-14 20:32:23 +0300

    2 версия 10 ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+
 import java.time.LocalDate;
 
 @Data
-public class Film {                 //Добрый день! если аннотирую все поля аннотациями типа @NonNull по доп заданию то постман не проходит,если без аннотаций классов model - все ОК, я не верно аннатриую или проверки постман не расщитаны на использование данных аннотаций
-    Long id;
-    String name;
-    String description;
-    LocalDate releaseDate;
-    Integer duration;
+public class Film {
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private Integer duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 
 @Data
 public class User {
-    Long id;
-    String email;
-    String login;
-    String name;
-    LocalDate birthday;
+    private Long id;
+    private String email;
+    private String login;
+    private String name;
+    private LocalDate birthday;
 }
